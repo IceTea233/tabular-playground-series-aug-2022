29cc092fb5da021e84915ab9722f7bfb132e3df9
IceTea233 <evan85910907@gmail.com>
2023-01-08 23:03:28 +0800

    feat: add training

--- train.py
+++ train.py
@@ -3,6 +3,11 @@ import numpy as np
 import sklearn
 import utils
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import KFold
+from sklearn.metrics import accuracy_score
 
 TRAIN_DATA_PATH = 'train.csv'
 TRAIN_SIZE = 0.8
@@ -15,8 +20,21 @@ def preprocess():
 
     return features, labels
 
+def train(X, Y):
+    adb = AdaBoostClassifier(DecisionTreeClassifier(min_samples_split=10, max_depth=4),n_estimators=10,learning_rate=0.6)
+    adb.fit(X, Y)
+    return adb
+
 if __name__ == '__main__':
+    print('Preprocessing...')
     features, labels = preprocess()
     
-    X_train, X_val, Y_train, Y_val = train_test_split(features, labels, train_size=TRAIN_SIZE)
-    print(X_val)
+    print('Splitting data...')
+    X_train, X_val, Y_train, Y_val = train_test_split(features.values, labels.values, train_size=TRAIN_SIZE)
+    
+    print('training...')
+    model = train(X_train, Y_train)
+    acc = model.score(X_val, Y_val)
+    print('acc = ', acc)
+    print('result: ')
+    print(model.predict_proba(X_val))
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame as df
 from sklearn.preprocessing import OneHotEncoder
+import matplotlib.pyplot as plt
 
 def one_hot_encode(df):
     data_dum = pd.get_dummies(df)
@@ -16,3 +17,22 @@ def load_data(file_path: str):
     labels = data['failure']
     features = data.drop(labels=['failure', 'id'], axis="columns")
     return features, labels
+
+def plot_matrix(acc_grid, gamma_list, c_list):
+    fig, ax = plt.subplots()
+
+    ax.set_xlabel('Gamma parameter')
+    ax.set_ylabel('C parameter')
+    ax.set_xticks(ticks=np.arange(gamma_list.size), labels=gamma_list, rotation=45)
+    ax.set_yticks(ticks=np.flip(np.arange(c_list.size)), labels=np.flip(c_list))
+
+    for i in range(c_list.size):
+        for j in range(gamma_list.size):
+            ax.text(i, j, round(acc_grid[j][i], 3),
+                color='white' if acc_grid[j][i] < 0.5 else 'black', 
+                ha='center', va='center')
+
+    im = ax.imshow(acc_grid, origin='lower', cmap='cividis', interpolation='none')
+
+    fig.colorbar(im, orientation='vertical')
+    plt.show()

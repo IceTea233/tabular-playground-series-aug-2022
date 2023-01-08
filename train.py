fbd9e2f0a914e8bea61a0ffbe85914b93585b33e
IceTea233 <evan85910907@gmail.com>
2023-01-08 17:39:32 +0800

    feat: add data splitting
    - And fix the problem of order of features/labels

--- train.py
+++ train.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import sklearn
 import utils
+from sklearn.model_selection import train_test_split
 
 TRAIN_DATA_PATH = 'train.csv'
+TRAIN_SIZE = 0.8
 
 def preprocess():
-    labels, features = utils.load_data(TRAIN_DATA_PATH)
+    features, labels = utils.load_data(TRAIN_DATA_PATH)
     features = utils.one_hot_encode(features)
     features = utils.fill_NaN_with_mean(features)
     labels = utils.fill_NaN_with_mean(labels)
@@ -13,4 +16,7 @@ def preprocess():
     return features, labels
 
 if __name__ == '__main__':
-    features, label = preprocess()
+    features, labels = preprocess()
+    
+    X_train, X_val, Y_train, Y_val = train_test_split(features, labels, train_size=TRAIN_SIZE)
+    print(X_val)
